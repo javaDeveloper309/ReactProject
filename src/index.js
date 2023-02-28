13a46b7db58527745936a65d728c659546478e81
Siddharth <siddharth.malvi23@gmail.com>
2023-02-28 19:41:07 +0530

    this is new commit by siddharth

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const sidd={siddharth};
 root.render(
   <React.StrictMode>
     <App />
